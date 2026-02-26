00aaeeffce2478e80b5a29f73c085810d2af7540
kairoshdz <erickdhernandez@lewisu.edu>
2026-02-25 19:21:07 -0600

    getting test website to work

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require('express')
 const app = express()
 const cors = require("cors");
+const path = require("path");
 app.use(cors());
 
 const port = process.env.PORT || 3000
 
-const path = require("path");
-app.get("/test", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('Diceroller API active')
-})
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/api/roll', (req, res) => {
     const roll = Math.floor(Math.random() * 6) + 1
